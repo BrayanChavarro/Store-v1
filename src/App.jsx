30885a6deca830ed949d45ed0304c15c306837ba
Brayan Chavarro <brayanstik.21@outlook.com>
2024-05-22 16:55:16 -0500

    Primer-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import React from "react";
+export function App() {
+  return (
+    <>
+      <header>
+        
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
